4a4f992e59b980b8e1bceee9060e278d4f4fe5f5
Simonsator <00pflaume@gmail.com>
2022-11-15 17:13:10 +0100

    Use bootstrap

--- src/view/no_request_send.php
+++ src/view/no_request_send.php
@@ -1,24 +1,24 @@
 <?php require_once 'src/view/html/header.html'; ?>
-<form method='get'>
+<form method='get' class="form-inline">
     <label>
         Find player by player name:
-        <input type='text' max='16' placeholder='Player name' name='name'>
+        <input type='text' max='16' placeholder='Player name' name='name' class="form-control">
     </label>
-    <input type='submit' name='submit'>
+    <input type='submit' name='submit' class="btn btn-primary mb-2">
 </form>
-<form method='get'>
+<form method='get' class="form-inline">
     <label>
         Find player by UUID:
         <input type='text' minlength='36' maxlength='36' name='uuid'
-               placeholder='Player UUID'>
+               placeholder='Player UUID' class="form-control">
     </label>
-    <input type='submit' name='submit'>
+    <input type='submit' name='submit' class="btn btn-primary mb-2">
 </form>
-<form method='get'>
+<form method='get' class="form-inline">
     <label>
         Find player by ID:
-        <input type='number' name='id' placeholder='Player id'>
+        <input type='number' name='id' placeholder='Player id' class="form-control">
     </label>
-    <input type='submit' name='submit'>
+    <input type='submit' name='submit' class="btn btn-primary mb-2">
 </form>
 <?php require_once 'src/view/html/body_end.html'; ?>
